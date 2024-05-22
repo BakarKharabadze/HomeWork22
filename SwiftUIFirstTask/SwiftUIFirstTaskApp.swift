a2a75b6d042dbb29d0503a2d32b8508f85a85808
BakarKharabadze <kharabadzebaqar@gmail.com>
2024-05-22 10:00:15 +0400

    Initial Commit

---
+++ SwiftUIFirstTask/SwiftUIFirstTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirstTaskApp.swift
+//  SwiftUIFirstTask
+//
+//  Created by Bakar Kharabadze on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirstTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
